85e9be7b538ecc43006b58c114d0db02c588d22e
isletspace <liewzheng@qq.com>
2026-07-17 14:47:56 +0800

    review-engine 0.7.8

--- Formula/review-engine.rb
+++ Formula/review-engine.rb
@@ -1,34 +1,34 @@
 class ReviewEngine < Formula
   desc "A virtual CodeReview Board for your code — multi-expert, scored, and actionable"
   homepage "https://github.com/Liewzheng/ReviewEngine"
-  version "0.7.6"
+  version "0.7.8"
   license "Apache-2.0"
 
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.6/review-engine-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "23718175791ee0c13bea926bfa376c7ee29d42c19e6a8e488d97a1ab89ec4ae5"
+      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.8/review-engine-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2aa07b919f5151431b6a2380754eea1feacf0b914622ac560595838498c3004d"
     end
   end
 
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.6/review-engine-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a0d1eb45f0db9b2ce5617381b0d598975fdcc868bd610683a1314a151cb25e85"
+      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.8/review-engine-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "afdfb69fdf6fcea680a167bdd2622776a24a9e4c840fb2a0ca48516c92b3bfbf"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.6/review-engine-aarch64-apple-darwin.tar.gz"
-      sha256 "18751cda04e5e9224f6cb34bfedaf6917463f0fb8289e6779f1f54478140a864"
+      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.8/review-engine-aarch64-apple-darwin.tar.gz"
+      sha256 "6e8493b23e1a38cfac486e74e6a648ddd2beb29a1efa38c274d67cea40467e87"
     end
   end
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.6/review-engine-x86_64-apple-darwin.tar.gz"
-      sha256 "3205a188b8c3587cd05b38f9b70142566ef2a16267d3e11f940eb3cff91fa8d6"
+      url "https://github.com/Liewzheng/ReviewEngine/releases/download/v0.7.8/review-engine-x86_64-apple-darwin.tar.gz"
+      sha256 "68935b2f03f43d9bcd2f5ccb4f22738d34e49c9815dad3fe4408280d555e393f"
     end
   end
 
